d91d230f07f29fcddfeac9b2e1a55d861cda47eb
Oscar Raygoza <oscar.eduardo.raygoza@gmail.com>
2022-05-02 11:52:59 -0500

    DELEATE: composition api

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,5 @@
 <script>
 /*
-* Composition API
-
-import { ref, watchEffect } from 'vue'
-
-const API_URL = `https://api.github.com/repos/Oscar-Raygoza/vue-github-commits/commits?per_page=3&sha=`
-const branches = ['main', 'v2-compat']
-
-const currentBranch = ref(branches[0])
-const commits = ref(null)
-
-watchEffect(async () => {
-  // this effect will run immediately and then
-  // re-run whenever currentBranch.value changes
-  const url = `${API_URL}${currentBranch.value}`
-  commits.value = await (await fetch(url)).json()
-})
-
-function truncate(v) {
-  const newline = v.indexOf('\n')
-  return newline > 0 ? v.slice(0, newline) : v
-}
-
-function formatDate(v) {
-  return v.replace(/T|Z/g, ' ')
-}
-*/
-
-/*
  * Options API
  */
 
